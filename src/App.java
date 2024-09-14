e37bbe512d6e54d26fcdc4b9a5634583fffd3cd4
Joskari98 <amk1005432@student.hamk.fi>
2024-09-14 19:11:12 +0300

    Lukujen arvot

--- src/App.java
+++ src/App.java
@@ -1,8 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        float luku1 = 5;
+        float luku2 = 7;
+        float tulo = luku1 * luku2;
+
         System.out.println("Hei olen Tulostin-ohjelma");
         String tekijä = "Joni Leinonen";
         System.out.println("Ohjelman tekijä: " + tekijä);
+
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku2-muuttujan arvo on " + luku2);
+
     
     }
 
