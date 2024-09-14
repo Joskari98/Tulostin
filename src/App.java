1b4834329660a870616ca496e7c620706b7382f9
Joskari98 <amk1005432@student.hamk.fi>
2024-09-14 19:07:56 +0300

    Tekstin tulostus

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hei olen Tulostin-ohjelma");
+        String tekijä = "Joni Leinonen";
+        System.out.println("Ohjelman tekijä: " + tekijä);
+    
     }
+
 }
